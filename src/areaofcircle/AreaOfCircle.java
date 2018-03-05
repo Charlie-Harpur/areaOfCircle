4c56a5d34d69eeb64a84f9b4cf165511c8fcb98b
chhar9972 <chhar9972@7L1M6Y1.Student.UGDSB.ED>
2018-03-05 12:47:11 -0500

    Made the thing

---
+++ src/areaofcircle/AreaOfCircle.java
@@ -0,0 +1,17 @@
+/*
+ * Charlie Harpur
+ * Calculates and prints the area of a circle with a radius of 15cm
+ * 28/02/2018
+ */
+
+package areaofcircle;
+
+public class AreaOfCircle
+{
+    public static void main(String[] args)
+    {
+        double area = (3.14159 * 15 * 15);
+        System.out.println ("The area of a circle with a radius of 15cm is " + area + "cm squared.");
+    }
+    
+}
